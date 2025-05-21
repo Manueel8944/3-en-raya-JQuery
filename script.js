1f059b7753883910b1267616840f19af5e070706
Manueel8944 <manueel8944@gmail.com>
2025-05-21 20:20:03 +0200

    acabado

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ $(document).ready(function () {
     let ganadoras = [
         [0,1,2], [3,4,5], [6,7,8],
         [0,3,6], [1,4,7], [2,5,8],
-        [0,4,7], [2,4,6]
+        [0,4,8], [2,4,6]
     ]
+
+    let victoriasx = 0
+    let victoriaso = 0
 
     function cambiarTurno(){
 
@@ -33,16 +36,19 @@ $(document).ready(function () {
 
     function comprobarGanador(){
         let ganador
-
+        let indexGanadoras
         for(let i = 0; i < ganadoras.length; i++){
             
             let [a, b, c] = ganadoras[i]
             if (tablero[a] != " " && tablero[a] == tablero[b] && tablero[a] == tablero [c]) {
                 ganador = tablero[a]
+                indexGanadoras = ganadoras[i]
             }
         }
 
-        return ganador
+        let win = {0: ganador, 1: indexGanadoras}    
+
+        return win
     }
 
     $(".item").on("click", function(){
@@ -52,7 +58,44 @@ $(document).ready(function () {
         if(comprobarCelda(indice)){
             $(this).text(turnos[turnoActual])
             tablero[indice] = turnos[turnoActual]
-            cambiarTurno()
+
+            if(comprobarGanador()[0] == "x") {
+                let celdas = comprobarGanador()[1];
+                $("#" + celdas[0]).css("background-color", "rgb(75, 192, 75)");
+                $("#" + celdas[1]).css("background-color", "rgb(75, 192, 75)");
+                $("#" + celdas[2]).css("background-color", "rgb(75, 192, 75)");
+                tablero = ["", "", "", "", "", "" , "", "", ""]
+                victoriasx++
+                $("#contx").text("x: " + victoriasx)
+                $("#contx").css("background-color", "rgb(75, 192, 75)")
+                cambiarTurno()
+
+            }
+
+            else if(comprobarGanador()[0] == "o"){
+                let celdas = comprobarGanador()[1];
+                $("#" + celdas[0]).css("background-color", "rgb(75, 192, 75)");
+                $("#" + celdas[1]).css("background-color", "rgb(75, 192, 75)");
+                $("#" + celdas[2]).css("background-color", "rgb(75, 192, 75)");
+                tablero = ["", "", "", "", "", "" , "", "", ""]
+                victoriaso++
+                $("#conto").text("o: " + victoriaso)
+                $("#conto").css("background-color", "rgb(75, 192, 75)")
+                cambiarTurno()
+            }
+
+            else{
+                console.log(tablero)
+                cambiarTurno()
+            }
         }
+    })
+
+    $(".reset").on("click",function(){
+        tablero = [" ", " ", " ", " ", " ", " " , " ", " ", " "]
+        $(".item").text("")
+        $(".item").css("background-color", "white");
+        $("#conto").css("background-color", "white")
+        $("#contx").css("background-color", "white")
     })
 })
